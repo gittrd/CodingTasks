408d74819b9495e4bdad52a91cec36c50b9107d8
Razvan_D <91324108+gittrd@users.noreply.github.com>
2024-06-09 10:11:44 +0100

    Add files via upload

---
+++ DataTypes_ConditionalStatements/numers.py
@@ -0,0 +1,38 @@
+'''
+Auto-graded Task 2
+Follow these steps:
+● Create a new Python file called numbers.py.
+● Ask the user to enter three different integers.
+● Then print out:
+○ The sum of all the numbers
+○ The first number minus the second number
+○ The third number multiplied by the first number
+○ The sum of all three numbers divided by the third number
+'''
+
+'''
+Prompt the user to type in three different types of integers
+Then print out the sum of all the numbers
+Then the first number minus the second number
+Then the third number multiplied by the first number
+Then the sum of all three numbers divided by the third number
+'''
+
+# prompting the user to input three different types of integers
+integer_1 = 21
+integer_2 = 7
+integer_3 = 92
+
+# obtaining the sum of all integers
+total = integer_1 + integer_2 + integer_3
+print(total)
+
+# computations
+comp_1 = integer_1 - integer_2
+print(comp_1)
+
+comp_2 = integer_3 * integer_1
+print(comp_2)
+
+comp_3 = total / integer_3
+print(comp_3)
